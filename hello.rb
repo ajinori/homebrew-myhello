3cb567b1cf2cbfdf3cd45bd6e64a673219a5c562
takahiro <takahiro600@gmail.com>
2014-05-11 01:28:28 +0900

    add hello.rb

---
+++ hello.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class Hello < Formula
+	homepage 'https://github.com/takahiro600/hello/'
+	url 'https://github.com/takahiro600/hello.git'
+	version '0.0.1'
+
+	def install
+		bin.install "hello"
+	end
+end
